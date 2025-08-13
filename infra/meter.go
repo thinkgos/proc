b69ce92aa81ef915573fb5629937eba1a4273a03
thinkgo <thinkgo@aliyun.com>
2025-08-13 15:18:37 +0800

    feat: add meter

---
+++ infra/meter.go
@@ -0,0 +1,157 @@
+package infra
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// Storage unit constants.
+const (
+	Byte  = 1
+	KByte = Byte << 10
+	MByte = KByte << 10
+	GByte = MByte << 10
+	TByte = GByte << 10
+	PByte = TByte << 10
+	EByte = PByte << 10
+)
+
+// ByteSize byte size
+type ByteSize uint64
+
+// Bytes to byte
+func (b ByteSize) Bytes() uint64 {
+	return uint64(b)
+}
+
+// KBytes to kilobyte
+func (b ByteSize) KBytes() float64 {
+	return float64(b/KByte) + float64(b%KByte)/float64(KByte)
+}
+
+// MBytes to megabyte
+func (b ByteSize) MBytes() float64 {
+	return float64(b/MByte) + float64(b%MByte)/float64(MByte)
+}
+
+// GBytes to gigabyte
+func (b ByteSize) GBytes() float64 {
+	return float64(b/GByte) + float64(b%GByte)/float64(GByte)
+}
+
+// TBytes to terabyte
+func (b ByteSize) TBytes() float64 {
+	return float64(b/TByte) + float64(b%TByte)/float64(TByte)
+}
+
+// PBytes to petabyte
+func (b ByteSize) PBytes() float64 {
+	return float64(b/PByte) + float64(b%PByte)/float64(PByte)
+}
+
+// EBytes to ebyte
+func (b ByteSize) EBytes() float64 {
+	return float64(b/EByte) + float64(b%EByte)/float64(EByte)
+}
+
+// String to string like xxB,xxKB,xxMB,xxGB,xxTB,xxPB,xxEB
+func (b ByteSize) String() string {
+	if b < 10 {
+		return fmt.Sprintf("%dB", b)
+	}
+	sizes := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	e := math.Floor(math.Log(float64(b)) / math.Log(1024))
+	val := float64(b) / math.Pow(1024, e)
+	return fmt.Sprintf("%0.2f%s", val, sizes[int(e)])
+}
+
+// HumanSize human readable string
+func (b ByteSize) HumanSize() string {
+	return b.String()
+}
+
+// MarshalText marshal to text
+func (b ByteSize) MarshalText() ([]byte, error) {
+	return []byte(b.String()), nil
+}
+
+// UnmarshalText unmarshal to ByteSize
+func (b *ByteSize) UnmarshalText(t []byte) error {
+	var i int
+
+	// backup for error message
+	t0 := t
+
+	for ; i < len(t); i++ {
+		if c := t[i]; !((c >= '0' && c <= '9') || c == '.') {
+			if i == 0 {
+				*b = 0
+				return &strconv.NumError{
+					Func: "UnmarshalText",
+					Num:  string(t0),
+					Err:  strconv.ErrSyntax,
+				}
+			}
+			break
+		}
+	}
+
+	val, err := strconv.ParseFloat(string(t[:i]), 64)
+	if err != nil {
+		return &strconv.NumError{
+			Func: "UnmarshalText",
+			Num:  string(t0),
+			Err:  err,
+		}
+	}
+
+	unit := uint64(Byte)
+	unitStr := strings.ToLower(strings.TrimSpace(string(t[i:])))
+	switch unitStr {
+	case "", "b", "byte": // do nothing
+	case "k", "kb", "kilo", "kilobyte", "kilobytes":
+		unit = KByte
+	case "m", "mb", "mega", "megabyte", "megabytes":
+		unit = MByte
+	case "g", "gb", "giga", "gigabyte", "gigabytes":
+		unit = GByte
+	case "t", "tb", "tera", "terabyte", "terabytes":
+		unit = TByte
+	case "p", "pb", "peta", "petabyte", "petabytes":
+		unit = PByte
+	case "e", "eb", "exa", "exabyte", "exabytes":
+		unit = EByte
+	default:
+		*b = 0
+		return &strconv.NumError{
+			Func: "UnmarshalText",
+			Num:  string(t0),
+			Err:  strconv.ErrSyntax,
+		}
+	}
+
+	if uint64(val) > math.MaxUint64/unit {
+		*b = ByteSize(math.MaxUint64)
+		return &strconv.NumError{
+			Func: "UnmarshalText",
+			Num:  string(t0),
+			Err:  strconv.ErrRange,
+		}
+	}
+	*b = ByteSize(val * float64(unit))
+	return nil
+}
+
+// ParseBytes parse a human string to byte
+func ParseBytes(s string) (uint64, error) {
+	v := ByteSize(0)
+	err := v.UnmarshalText([]byte(s))
+	return v.Bytes(), err
+}
+
+// HumanSize human readable string
+func HumanSize(bytes uint64) (s string) {
+	return ByteSize(bytes).HumanSize()
+}
---
+++ infra/meter_test.go
@@ -0,0 +1,120 @@
+package infra
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"testing"
+)
+
+func issFloatEqual(x, y float64) bool {
+	delta := math.Abs(x - y)
+	mean := math.Abs(x+y) / 2.0
+	return delta/mean < 0.000001
+}
+
+func TestByteSize(t *testing.T) {
+	if got, want := ByteSize(100).Bytes(), uint64(100); got != want {
+		t.Errorf("Bytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*12).KBytes(), 12.0; !issFloatEqual(got, want) {
+		t.Errorf("KBytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*1024*12).MBytes(), 12.0; !issFloatEqual(got, want) {
+		t.Errorf("MBytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*1024*1024*12).GBytes(), 12.0; !issFloatEqual(got, want) {
+		t.Errorf("GBytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*1024*1024*1024*12).TBytes(), 12.0; !issFloatEqual(got, want) {
+		t.Errorf("TBytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*1024*1024*1024*1024*12).PBytes(), 12.0; !issFloatEqual(got, want) {
+		t.Errorf("PBytes() = %v, want %v", got, want)
+	}
+	if got, want := ByteSize(1024*1024*1024*1024*1024*1024*13).EBytes(), 13.0; !issFloatEqual(got, want) {
+		t.Errorf("EBytes() = %v, want %v", got, want)
+	}
+	got, err := ByteSize(5).MarshalText()
+	if err != nil {
+		t.Errorf("MarshalText() Err = %v, want %v", got, false)
+	}
+	if want := []byte("5B"); !bytes.Equal(got, want) {
+		t.Errorf("MarshalText() = %v, want %v", got, want)
+	}
+}
+
+func TestParseBytes(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    uint64
+		wantErr bool
+	}{
+		{"5B", args{"5B"}, 5, false},
+		{"B", args{"100.0B"}, 100, false},
+		{"KB", args{"100.0KB"}, 1024 * 100, false},
+		{"MB", args{"100.0MB"}, 1024 * 1024 * 100, false},
+		{"GB", args{"100.0GB"}, 1024 * 1024 * 1024 * 100, false},
+		{"TB", args{"100.0TB"}, 1024 * 1024 * 1024 * 1024 * 100, false},
+		{"PB", args{"100.0PB"}, 1024 * 1024 * 1024 * 1024 * 1024 * 100, false},
+		{"EB", args{"10.0EB"}, 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 10, false},
+		{"no number", args{"b"}, 0, true},
+		{"invalid unit", args{"101.0cb"}, 0, true},
+		{"invalid number", args{"10.0.1B"}, 0, true},
+		{"value out of range", args{"1000.0EB"}, math.MaxUint64, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseBytes(tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseBytes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ParseBytes() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHumanSize(t *testing.T) {
+	type args struct {
+		bytes uint64
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantS string
+	}{
+		{"5B", args{5}, "5B"},
+		{"B", args{100}, "100.00B"},
+		{"KB", args{1024 * 100}, "100.00KB"},
+		{"MB", args{1024 * 1024 * 100}, "100.00MB"},
+		{"GB", args{1024 * 1024 * 1024 * 100}, "100.00GB"},
+		{"TB", args{1024 * 1024 * 1024 * 1024 * 100}, "100.00TB"},
+		{"PB", args{1024 * 1024 * 1024 * 1024 * 1024 * 100}, "100.00PB"},
+		{"EB", args{1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 3}, "3.00EB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotS := HumanSize(tt.args.bytes); gotS != tt.wantS {
+				t.Errorf("HumanSize() = %v, want %v", gotS, tt.wantS)
+			}
+		})
+	}
+}
+
+func ExampleHumanSize() {
+	value, _ := ParseBytes("2.99TB")
+	s := HumanSize(value)
+	fmt.Println(value)
+	fmt.Println(s)
+
+	// output:
+	// 3287539767050
+	// 2.99TB
+}
