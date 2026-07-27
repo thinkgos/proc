aa5766fb5c608c274766848ce015a417eaa34f42
thinkgo <thinkgo@aliyun.com>
2026-07-28 07:52:49 +0800

    refactor: refactor password

--- password/password.go
+++ password/password.go
@@ -2,7 +2,6 @@ package password
 
 import (
 	"errors"
-	"sync/atomic"
 )
 
 type CryptFacade interface {
@@ -16,24 +15,3 @@ const DefaultSaltSize = 16
 
 // ErrCompareFailed compare failed
 var ErrCompareFailed = errors.New("crypt compare failed")
-
-// 考虑有些场景是一些低性能机器, 在使用bcrypt时, 很耗时
-var crypt = &atomic.Pointer[CryptFacade]{}
-
-func init() {
-	SetCrypt(NewBcrypt(0))
-}
-
-func SetCrypt(pf CryptFacade) {
-	crypt.Store(&pf)
-}
-
-// GenerateFromPassword password hash encryption
-func GenerateFromPassword(password string) (string, error) {
-	return (*crypt.Load()).GenerateFromPassword(password)
-}
-
-// CompareHashAndPassword password hash verification
-func CompareHashAndPassword(hashedPassword, password string) error {
-	return (*crypt.Load()).CompareHashAndPassword(hashedPassword, password)
-}
